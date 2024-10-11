4db4f8a699dac3d66f85a47b706d45f41b76a7f2
francescomerighi1202 <francescomerighi61@gmail.com>
2024-10-11 19:29:28 +0200

    Inizio progetto

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,4 @@
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -19,9 +7,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html lang="it">
+      <head>
+        <title>FM Web Agency</title>
+      </head>
+      <body>
+        <h1>FM WEB AGENCY</h1>
       </body>
     </html>
   );
